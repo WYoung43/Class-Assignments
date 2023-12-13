f3f10b7d1ecf868c256b0db104b7ad8febbbb0a2
William Young <wyoung.prsvr@gmail.com>
2023-12-12 19:49:44 -0600

    Discription of your commit

---
+++ Class Notes/Week 5/Day 1/function.js
@@ -0,0 +1,73 @@
+// /////////////////////////////////Functions//////////////////////////////////////////////////
+//Buliding block of your JS code
+//it is a reusable block of code allow you to perform a specific task
+// Function Declaration
+function functionName(){
+    //code goes here
+}
+
+functionName() //calling the function into action 
+
+function greetingsGenerator(name){
+    console.log("hello"+ " "+ name)
+}
+
+greetingsGenerator("Kelebet")
+
+// Function without a parameter and return
+
+// function add(){
+//     let num1=5;
+//     let num2=5;
+//     let sum=num1+num2
+//     console.log(sum)
+// }
+
+// add()
+// Function returning value
+function add(){
+    let num1=5;
+    let num2=5;
+    let sum=num1+num2
+    return sum
+}
+
+console.log(add())
+
+
+// Function with a parameter
+// function greetingsGenerator(name){
+//     console.log("hello"+ " "+ name)
+// }
+
+// greetingsGenerator("Kelebet")
+// Function with two parameters
+// function adding(num1, num2){
+//     console.log(num1+num2)
+// }
+
+// adding(555, 89)
+// Function with many parameters
+function adding(num1, num2, num3, num4){
+    console.log(num1+num2+num3+num4)
+}
+
+adding(555, 89,45,48)
+//......................................To Do................................................
+// Declare a function fullName and it print out your full name.
+
+
+// Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+
+
+// Declare a function addNumbers and it takes two two parameters and it returns sum.
+
+
+// Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
+
+
+// Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+// ["us", "canada","barcelona"]
+
+// return ["US", "CANADA", "BARCELONA"]
